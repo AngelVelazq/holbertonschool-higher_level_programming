12ad9182043f2e644f676fb651ae1af21556a869
AngelVelazq <angelvelazquez.op@gmail.com>
2024-05-08 22:10:52 -0400

    prints a string in uppercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -8,4 +8,5 @@ def uppercase(s):
             # ASCII values of lowercase letters are 32 greater than uppercase
         else:
             result += char
-    print(result)
+
+            print(result)
