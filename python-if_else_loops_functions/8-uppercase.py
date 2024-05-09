88016f37f2b84f198139beaff7945a5824a74671
AngelVelazq <angelvelazquez.op@gmail.com>
2024-05-08 22:04:30 -0400

    prints a string in uppercase

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uppercase(s):
+    result = ""
+    for char in s:
+        if 'a' <= char <= 'z':
+            # Convert lowercase to uppercase using ASCII values
+            result += chr(ord(char) - 32)
+            # ASCII values of lowercase letters are 32 greater than uppercase
+        else:
+            result += char
+            print(result)
