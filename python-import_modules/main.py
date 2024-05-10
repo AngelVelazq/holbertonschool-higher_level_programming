a7d6d8f477e61bc7e9a17827e69bbfb36599a6f2
AngelVelazq <angelvelazquez.op@gmail.com>
2024-05-10 17:30:21 -0400

    import function and print the result

--- python-import_modules/main.py
+++ python-import_modules/main.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-from add_0 import add as FAKE_add
+from 0-add import add as FAKE_add
 
 a = 1
 b = 2
 
 # Print the result using string formatting
 print("a = {} and b = {} FAKE add() => {}".format(a, b, FAKE_add(a, b)))
+
+if __name__ == "__main__":
+    pass
