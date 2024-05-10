36c734fa67b73da21a5267ddaad71744e28a3d32
AngelVelazq <angelvelazquez.op@gmail.com>
2024-05-10 18:38:07 -0400

    imports functions that does math

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
-from calculator_1 import (
-        add as FAKE_add,
-        sub as FAKE_add,
-        mul as FAKE_add,
-        div as FAKE_add,
-        )
+from calculator_1 import add, sub, mul, div
 
-a = 10
-b = 5
+if __name__ == "__main__":
+    a = 10
+    b = 5
 
-add_result = FAKE_add(a, b)
-sub_result = FAKE_sub(a, b)
-mul_result = FAKE_mul(a, b)
-div_result = FAKE_div(a, b)
+add_result = add(a, b)
+sub_result = sub(a, b)
+mul_result = mul(a, b)
+div_result = div(a, b)
 
 print("{} + {} = {}".format(a, b, add_result))
 print("{} - {} = {}".format(a, b, sub_result))
 print("{} * {} = {}".format(a, b, mul_result))
 print("{} / {} = {}".format(a, b, div_result))
+
