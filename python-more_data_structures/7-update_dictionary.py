d73793fe4b8024914bff6219055e34b9f10f16fc
AngelVelazq <angelvelazquez.op@gmail.com>
2024-05-14 18:17:45 -0400

    replaces or adds key/value in a dictionary

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,15 +1,5 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    # Check if the key exists in the dictionary
-    if key in a_dictionary:
-        # If the key exists, update its value
-        a_dictionary[key] = value
-    else:
-        # If the key doesn't exist, add it to the dictionary
-        a_dictionary[key] = value
-
-
-my_dict = {'a': 1, 'b': 2, 'c': 3}  # Test cases
-update_dictionary(my_dict, 'a', 10)  # Update existing key
-update_dictionary(my_dict, 'd', 4)   # Add new key
-print(my_dict)  # Output: {'a': 10, 'b': 2, 'c': 3, 'd': 4}
+    """Updates or adds a key/value pair in a dictionary."""
+    a_dictionary[key] = value
+    return a_dictionary
