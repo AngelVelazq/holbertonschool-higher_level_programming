862de197d751f913ebce0155cb52f74607770c3a
AngelVelazq <angelvelazquez.op@gmail.com>
2024-05-15 17:19:19 -0400

    print elements of a list

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -10,8 +10,3 @@ def safe_print_list(my_list=[], x=0):
     finally:
         print()
     return printed_elements
-
-
-my_list = [1, 2, 3, 4, 5]  # Example usage:
-x = 3
-print("Number of elements printed:", safe_print_list(my_list, x))
