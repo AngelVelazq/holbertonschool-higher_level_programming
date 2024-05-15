dfc57fc526c49b0318cf58914f5c356c8afe2426
AngelVelazq <angelvelazquez.op@gmail.com>
2024-05-15 18:40:44 -0400

    function that prints x elements of a list

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -15,10 +15,3 @@ def safe_print_list(my_list=[], x=0):
     except Exception as e:
         print("An error occurred:", e)
         return count
-
-
-# Test the function
-my_list = [1, 2, 3, 4, 5]
-x = 3
-num_printed = safe_print_list(my_list, x)
-print("Number of elements printed:", num_printed)
