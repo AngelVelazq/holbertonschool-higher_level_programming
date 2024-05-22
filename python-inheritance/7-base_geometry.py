65734181314ac00f0ce8f9d6f464b71582d94c96
AngelVelazq <angelvelazquez.op@gmail.com>
2024-05-22 18:20:21 -0400

    Public instance method: def integer_validator(self, name, value)

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,32 +1,16 @@
 #!/usr/bin/python3
-"""
-This module contains the definition of the BaseGeometry class."""
+'''Module for BaseGeometry class.'''
 
 
 class BaseGeometry:
-    """
-    A class representing basic geometry.
-    """
-
+    '''A BaseGeometry class.'''
     def area(self):
-        """
-        Raises an Exception with the message "area() is not implemented".
-        """
-        raise Exception("area() is not implemented")
+        '''Method to compute this area.'''
+        raise Exception('area() is not implemented')
 
     def integer_validator(self, name, value):
-        """
-        Validates the value.
-
-        Args:
-            name : The name of the value being validated.
-            value: The value to be validated.
-
-        Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is less than or equal to 0.
-        """
-        if not isinstance(value, int):
+        '''Method for validating the value.'''
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
