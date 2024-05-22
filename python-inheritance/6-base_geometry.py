38da636cff58a0fadeb2210f57768da66cd2ec3c
AngelVelazq <angelvelazquez.op@gmail.com>
2024-05-22 17:20:19 -0400

    public instance method with exception

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module contains the definition of the BaseGeometry class."""
+
+
+class BaseGeometry:
+    """
+    A class representing basic geometry.
+    """
+
+    def area(self):
+        """
+        Raises an Exception with the message "area() is not implemented".
+        """
+        raise Exception("area() is not implemented")
