ddea0fa4b7412f881a8b9199b60232a83f8cb212
AngelVelazq <angelvelazquez.op@gmail.com>
2024-05-29 20:59:03 -0400

    delete example usage

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -6,9 +6,8 @@ def serialize_and_save_to_file(data, filename):
         data (dict): Python dictionary containing the data to be serialized.
         filename (str): The filename of the output JSON file.
         If the file already exists, it will be replaced.
-
-    Returns:
-        None"""
+        Returns: None
+        """
     with open(filename, 'w') as file:
         json.dump(data, file)
 
@@ -25,26 +24,3 @@ def load_and_deserialize(filename):
     with open(filename, 'r') as file:
         data = json.load(file)
     return data
-
-
-# Example usage
-if __name__ == "__main__":
-    # Sample data to be serialized
-    data_to_serialize = {
-        "name": "John Doe",
-        "age": 30,
-        "city": "New York"
-    }
-
-    # Serialize the data to JSON and save it to a file
-    serialize_and_save_to_file(data_to_serialize, 'data.json')
-
-    # Output: The data has been serialized and saved to 'data.json'
-    print("Data serialized and saved to 'data.json'.")
-
-    # Load and deserialize data from 'data.json'
-    deserialized_data = load_and_deserialize('data.json')
-
-    # Output: The deserialized data
-    print("Deserialized Data:")
-    print(deserialized_data)
