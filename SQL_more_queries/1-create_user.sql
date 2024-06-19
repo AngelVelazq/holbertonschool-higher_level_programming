b39ea50e0dc499bcef4c3ebf811cb8d72e06b142
AngelVelazq <angelvelazquez.op@gmail.com>
2024-06-18 21:48:43 -0400

    creates the MySQL server user user_0d_1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -3,11 +3,5 @@
 -- Create user_0d_1 if it does not exist
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
--- Alter the user to ensure the password is set correctly (if the user already existed)
-ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
 -- Grant all privileges to user_0d_1
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-
--- Flush privileges to ensure the changes take effect
-FLUSH PRIVILEGES;
