018195445288f0441fa4c6d6014319edfcc4690d
AngelVelazq <angelvelazquez.op@gmail.com>
2024-06-18 23:57:15 -0400

    advanced

--- SQL_more_queries/100-not_my_genres.sql
+++ SQL_more_queries/100-not_my_genres.sql
@@ -1,19 +1,20 @@
--- List all genres not linked to the show Dexter in hbtn_0d_tvshows
+-- List all genres not linked to the show Dexter from database hbtn_0d_tvshows
 
--- Step 1: Get the genre_id linked to the show Dexter
-SELECT genre_id
-FROM tv_show_genres
-JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+-- Step 1: Get genres linked to Dexter
+SELECT tv_genres.id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_shows.title = 'Dexter';
 
--- Step 2: List all genres that are not linked to the genre_id obtained from Step 1
+-- Step 2: Get genres that are not linked to Dexter
 SELECT tv_genres.name
 FROM tv_genres
-LEFT JOIN (
-    SELECT DISTINCT genre_id
-    FROM tv_show_genres
-    JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_genres.id NOT IN (
+    SELECT tv_genres.id
+    FROM tv_shows
+    LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+    LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
     WHERE tv_shows.title = 'Dexter'
-) AS linked_genres ON tv_genres.id = linked_genres.genre_id
-WHERE linked_genres.genre_id IS NULL
-ORDER BY tv_genres.name ASC;
+)
+ORDER BY tv_genres.name;
