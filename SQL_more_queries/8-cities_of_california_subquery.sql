729edd6ec3c5ce762662a95c49416acbae8f1179
AngelVelazq <angelvelazquez.op@gmail.com>
2024-06-18 22:41:12 -0400

    lists all the cities of California

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,9 @@
+-- List all the cities of California
+SELECT cities.id, cities.name
+FROM cities
+WHERE cities.state_id = (
+    SELECT states.id
+    FROM states
+    WHERE states.name = 'California'
+)
+ORDER BY cities.id ASC;
