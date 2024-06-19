ded03592370d17f64056af54869de4d0c7bdc2f2
AngelVelazq <angelvelazquez.op@gmail.com>
2024-06-18 23:43:26 -0400

    based on datadump

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,5 @@
--- Retrieve the first 3 students from Batch ID=3
-SELECT id, name 
-FROM students 
-WHERE batch_id = 3 
-ORDER BY created_at DESC 
-LIMIT 3;
+-- List all shows and their genres (if any) from hbtn_0d_tvshows database
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
