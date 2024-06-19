daa691db47188704b815dfa82519f3785562260c
AngelVelazq <angelvelazquez.op@gmail.com>
2024-06-18 23:52:08 -0400

    all tasks based on dump from task 10 until here

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,6 @@
--- List all shows with their linked genres from hbtn_0d_tvshows
-SELECT tv_shows.title, tv_genres.name AS genre
+-- list all shows and all genres linked to that show from datbase htb...tvshow
+SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY tv_shows.title ASC, genre ASC;
+ORDER BY tv_shows.title, tv_genres.name;
