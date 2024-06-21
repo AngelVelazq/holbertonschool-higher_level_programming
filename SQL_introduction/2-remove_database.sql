68d586547a5f79c20b9605401721edbefe7443bb
AngelVelazq <angelvelazquez.op@gmail.com>
2024-06-20 21:09:34 -0400

    deletes the database

--- SQL_introduction/2-remove_database.sql
+++ SQL_introduction/2-remove_database.sql
@@ -1,17 +1,3 @@
--- Attempt to drop the database hbtn_0c_0 if it exists
+-- Drops the database hbtn_0c_0 if it exists.
+
 DROP DATABASE IF EXISTS hbtn_0c_0;
-
--- Check if the database was dropped successfully
--- This block will attempt to create a dummy table in the hbtn_0c_0 database.
--- If it succeeds, it means the database wasn't dropped, and an error is raised.
--- If it fails (due to the database not existing), it means the database was successfully dropped.
-DO $$
-BEGIN
-    DECLARE CONTINUE HANDLER FOR SQLSTATE '42S02' BEGIN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Database hbtn_0c_0 dropped successfully';
-    END;
-    
-    USE hbtn_0c_0;
-    CREATE TEMPORARY TABLE test_drop (id INT);
-END $$;
-
