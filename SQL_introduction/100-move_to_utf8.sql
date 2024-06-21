c53aa2b1e231e02c64cd335a62f8ce4c190fc121
AngelVelazq <angelvelazquez.op@gmail.com>
2024-06-20 21:37:42 -0400

    converts hbtn_0c_0 database to UTF8

---
+++ SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,9 @@
+-- Convert database to UTF8MB4
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Convert table first_table to UTF8MB4
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Convert field name in first_table to UTF8MB4
+ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
