8dc94fee1a8570acb4629edad307462a11da50f4
AngelVelazq <angelvelazquez.op@gmail.com>
2024-06-20 21:17:06 -0400

    prints the following description of the table

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,14 +1,3 @@
--- Script to print the description of the table first_table from the specified database
+-- SHOWS the create table statement
 
-SELECT 
-    COLUMN_NAME,
-    COLUMN_TYPE,
-    IS_NULLABLE,
-    COLUMN_KEY,
-    COLUMN_DEFAULT,
-    EXTRA
-FROM 
-    information_schema.COLUMNS
-WHERE 
-    TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'first_table';
-
+SHOW CREATE TABLE first_table
