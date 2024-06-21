86ff5a86b7d91043cba26abc8ec7abd5b3095d35
AngelVelazq <angelvelazquez.op@gmail.com>
2024-06-20 21:27:06 -0400

    lists all records of the table second_table

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,7 @@
+-- Script to list all records of the table second_table
+-- Displaying score and name, ordered by score in descending order
+
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
+
